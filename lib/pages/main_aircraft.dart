3249b3c69f379305bf010d695eb4ba46546091a5
jovan-aleksic <jovandevelopersrb@gmail.com>
2023-06-07 00:57:07 -0700

    fix loading feature

--- lib/pages/main_aircraft.dart
+++ lib/pages/main_aircraft.dart
@@ -36,13 +36,13 @@ class _MainAircraftState extends State<MainAircraft> {
   @override
   void initState() {
     super.initState();
-       contentBlockers.add(ContentBlocker(
-       trigger: ContentBlockerTrigger(
-         urlFilter: ".*",
-       ),
-       action: ContentBlockerAction(
-           type: ContentBlockerActionType.CSS_DISPLAY_NONE,
-           selector: ".root, .page-1, .page-last")));
+    contentBlockers.add(ContentBlocker(
+        trigger: ContentBlockerTrigger(
+          urlFilter: ".*",
+        ),
+        action: ContentBlockerAction(
+            type: ContentBlockerActionType.CSS_DISPLAY_NONE,
+            selector: ".root, .page-1, .page-last")));
 
 //     if (mounted) {
 //       _webViewController = WebViewController();
@@ -94,48 +94,49 @@ class _MainAircraftState extends State<MainAircraft> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () {
-        Navigator.of(context).pop();
-        return Future.value(true);
-      },
-      child: Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            Navigator.of(context).pop();
-            return Future.value(true);
-          },
-          backgroundColor: '#1E1F4D'.toColor(),
-          child: const Icon(Icons.keyboard_backspace),
-        ),
-        body: SafeArea(
-            child: isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                // : WebViewWidget(
-                //     controller: _webViewController!,
-                //   ),
-                : InAppWebView(
-                    initialUrlRequest: URLRequest(
-                        url: Uri.parse(
-                      UrlsOfWebsite.MAIN_AIRCRAFT_PAGE_URL,
-                    )),
-                    initialOptions: InAppWebViewGroupOptions(
-                        ios: IOSInAppWebViewOptions(),
-                        crossPlatform: InAppWebViewOptions(
-                            contentBlockers: contentBlockers)),
-                    onWebViewCreated: (controller) {
-                      webViewController = controller;
-                    },
-                    onProgressChanged: (InAppWebViewController controller, int progress){
-                      log("progress $progress");
-                      if(progress >= 100) {
-                        setState(() => isLoading = false);
-                      } else {setState(() => isLoading = false);}
-                    },
-                  )),
-      ),
-    );
+        onWillPop: () {
+          Navigator.of(context).pop();
+          return Future.value(true);
+        },
+        child: Scaffold(
+          floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              Navigator.of(context).pop();
+              return Future.value(true);
+            },
+            backgroundColor: '#1E1F4D'.toColor(),
+            child: const Icon(Icons.keyboard_backspace),
+          ),
+          body: SafeArea(
+              child: Stack(children: [
+            if (isLoading)
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
+            Expanded(
+              child: InAppWebView(
+                initialUrlRequest: URLRequest(
+                    url: Uri.parse(
+                  UrlsOfWebsite.MAIN_AIRCRAFT_PAGE_URL,
+                )),
+                initialOptions: InAppWebViewGroupOptions(
+                    ios: IOSInAppWebViewOptions(),
+                    crossPlatform:
+                        InAppWebViewOptions(contentBlockers: contentBlockers)),
+                onWebViewCreated: (controller) {
+                  webViewController = controller;
+                },
+                onProgressChanged: (controller, progress) =>
+                    {log("cur progress : $progress")},
+                onLoadStop: (InAppWebViewController controller, Uri? uri) {
+                  setState(() {
+                    isLoading = false;
+                  });
+                },
+              ),
+            )
+          ])),
+        ));
   }
 }
